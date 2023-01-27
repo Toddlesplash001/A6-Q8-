4534ad5b3b4904e74b770e08f9c75b8a8bba5450
Toddlesplash001 <118872908+Toddlesplash001@users.noreply.github.com>
2023-01-27 22:15:37 +0530

    Add files via upload

---
+++ A6 Q8.py
@@ -0,0 +1,26 @@
+def student_data(student_id, **kwargs):
+    print(f'\nStudent ID: {student_id}')
+    if 'student_name' in kwargs:
+        print(f"Student Name: $ {kwargs['student_name']}")
+    
+    if 'student_name' and 'student_class' in kwargs:
+            print(f"\nStudent Name: $ {kwargs['student_name']}")
+            print(f"Student Class: $ {kwargs['student_class']}")
+
+ 
+student_data(student_id='SV12', student_name='Jean Garner')
+
+student_data(student_id='SV12', student_name='Jean Garner', student_class ='V')
+def student_data(student_id, **kwargs):
+    print(f'\nStudent ID: {student_id}')
+    if 'student_name' in kwargs:
+        print(f"Student Name: $ {kwargs['student_name']}")
+    
+    if 'student_name' and 'student_class' in kwargs:
+            print(f"\nStudent Name: $ {kwargs['student_name']}")
+            print(f"Student Class: $ {kwargs['student_class']}")
+
+ 
+student_data(student_id='SV12', student_name='Jean Garner')
+
+student_data(student_id='SV12', student_name='Jean Garner', student_class ='V')
